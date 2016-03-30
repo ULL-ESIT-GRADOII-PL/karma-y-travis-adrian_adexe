61b3cb14994b03c0e92eb9ef96d7045c55ae7ec0
Adrian <alu0100288216@ull.edu.es>
2016-03-30 11:22:38 +0100

    Primer commit con .gitignore incluido y README.md modificado pero sin terminar.

---
+++ README.md
@@ -0,0 +1,38 @@
+# Práctica: Karma y Travis
+
+Práctica  donde partiendo de la aplicación de conversión de tipos, realizada con anterioridad utilizando los siguientes recursos:
+
+* Html
+* Css 
+* Javascript
+	+ POO 
+	+ herencia en javascript
+* Librería [**XRegExp**](http://xregexp.com/).
+* Pruebas con [**mocha**](http://mochajs.org/) y [**chai**](http://chaijs.com/)
+* Automatización de tareas con [**gulp**](http://gulpjs.com/)
+* Cubrimiento con [**blanketjs**](http://blanketjs.org/).
+
+Se le añadirá [**karma**](https://karma-runner.github.io/0.13/index.html) y [**travis**](https://travis-ci.org/).
+
+## Karma
+
+Karma se encarga de ejecutar los test de Javascript según se vayan construyendo, de tal forma que ante cualquier fallo el desarrollador se dará cuenta de inmediato.
+
+## Travis
+
+**Travis CI**, es un sistema distribuido de generación e integración continua libre que permite conectar tu repositorio de **gitHub** y probar después de cada *push* que hagas.
+
+## Repositorio GitHuB
+
+* [Repositorio Organización](https://github.com/ULL-ESIT-GRADOII-PL/karma-y-travis-adrian_adexe)
+
+## Página de la práctica
+
+* [Página Web de la práctica](http://ull-esit-gradoii-pl.github.io/karma-y-travis-adrian_adexe)
+* [Página Web de los tests](http://ull-esit-gradoii-pl.github.io/karma-y-travis-adrian_adexe/test)
+* [Enlace a la práctica en el campus virtual](https://campusvirtual.ull.es/1516/mod/workshop/view.php?id=185196)
+
+## Página del autor
+
+* [Adexe Sabina](http://alu0100769609.github.io/)
+* [Adrián Calzadilla](http://adcalzadilla.github.io/)
---
+++ test/tests.js
@@ -0,0 +1,82 @@
+var expect = chai.expect;
+
+describe("Creación de un objeto Medida", function(){
+   it("Debe tener un número", function(){
+      var medida = new Medida(23,'F');
+      expect(medida.valor).to.equal(23);
+   });
+   it("Debe tener una unidad de medida", function(){
+      var medida = new Medida(23,'F');
+      expect(medida.tipo).to.equal('F');
+   });
+   it("Debe aceptar una cadena del tipo: '23f'", function(){
+      var medida = new Medida('23F');
+      expect(medida.valor).to.equal('23');
+      expect(medida.tipo).to.equal('F');
+   });
+});
+
+describe("Creación de un objeto Temperatura", function(){
+   var temperatura = new Temperatura(23,'F');
+   it("Debe tener un número", function(){
+      expect(temperatura.valor).to.equal(23);
+   });
+   it("Debe tener una unidad de medida", function(){
+      expect(temperatura.tipo).to.equal('F');
+   });
+});
+
+describe("Creación de un objeto Kelvin", function(){
+   var kelvin = new Kelvin(2);
+   it("Debe tener un número", function(){
+      expect(kelvin.valor).to.equal(2);
+   });
+   it("Debe tener una unidad de medida", function(){
+      expect(kelvin.tipo).to.equal('k');
+   });
+});
+
+describe("Creación de un objeto Celsius", function(){
+   var celsius = new Celsius(3);
+   it("Debe tener un número", function(){
+      expect(celsius.valor).to.equal(3);
+   });
+   it("Debe tener una unidad de medida", function(){
+      expect(celsius.tipo).to.equal('c');
+   });
+});
+
+describe("Creación de un objeto Farenheit", function(){
+   var farenheit = new Farenheit(10);
+   it("Debe tener un número", function(){
+      expect(farenheit.valor).to.equal(10);
+   });
+   it("Debe tener una unidad de medida", function(){
+      expect(farenheit.tipo).to.equal('f');
+   });
+});
+
+describe("Conversión de temperaturas", function(){
+   describe("De celsius a kelvin", function(){
+      var celsius = new Celsius(23);
+      it("Valor", function(){
+         expect(celsius.toKelvin().valor).to.equal(296.15);
+      });
+      it("Tipo", function(){
+         expect(celsius.toKelvin().tipo).to.equal('k');
+      });
+   });
+   describe("De celsius a farenheit", function(){
+      var celsius = new Celsius(23);
+      it("Valor", function(){
+         expect(celsius.toFarenheit().valor).to.equal(73.4);
+      });
+      it("Tipo", function(){
+         expect(celsius.toFarenheit().tipo).to.equal('f');
+      });
+      it("Devuelve un objeto Farenheit", function(){
+         var far = celsius.toFarenheit();
+         expect(far instanceof Farenheit).to.equal(true);
+      });
+   });
+});
